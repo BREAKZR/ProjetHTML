75416d542da5bd0299426572d1ea3559b4b2c1a1
Florence Ewel <florence.ewel@student.uclouvain.be>
2019-10-16 12:30:47 +0200

    réorganisation de la hiérachie

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
